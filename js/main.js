c5268cbdf3b9806507b111fb68363a51b68129a9
MD. ABU BASAR <89003474+basar021@users.noreply.github.com>
2022-01-26 07:14:59 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,61 @@
+
+// header scroll
+
+let nav = document.querySelector(".navbar");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+
+
+// nav hide 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+});
+
+
+
+// owl carousel
+
+$(document).ready(function(){
+
+  $('.client-slider-section').owlCarousel({
+
+    margin: 20,
+    items:4,
+    loop: true,
+    autoplay: true,
+    nav:false,
+    // navText:[ $('.owl-navigation.owl-nav-prev'),$('.owl-navigation.owl-nav-next')] ,
+    autoplayTimeout: 2000,
+    responsive: {
+      0: {
+        items: 2
+    },
+    600: {
+        items: 3
+    },
+    1000: {
+        items: 6
+    }
+      
+    }
+
+
+  });
+
+
+
+  wow = new WOW();
+  wow.init();
+
+
+});
